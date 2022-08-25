56200425c83c97be08946482b714f8fcb5e84f44
Unipalette <wjdtmddbs16@naver.com>
2022-08-25 12:58:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [li, setList] = useState([]);
+  function onInput(e) {
+    const nextValue = e.target.value;
+    setInputValue(nextValue);
+  }
+  function onSubmit(e) {
+    // usestate 배열에 추가 하기
+    e.preventDefault();
+    /*   const newLi = [...li, inputValue];
+    setList(newLi);
+    li.map((todo) => {
+      return <li>{todo}</li>;
+    });
+    console.log(li); */
+    const toDoList = document.querySelector(".toDoList");
+    e.preventDefault();
+    const li = document.createElement("li");
+    li.innerText = inputValue;
+    toDoList.append(li);
+    setInputValue("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>안녕</h1>
+      <ul className="toDoList"></ul>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={inputValue} onChange={onInput} />
+        <button type="submit">확인</button>
+      </form>
+    </>
   );
 }
-
 export default App;
